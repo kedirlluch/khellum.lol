29561848ac7ef286f49fa956d1e6d74135677e0b
kedirlluch <kedir980210@gmail.com>
2026-01-21 00:30:01 +0000

    Created a websocket server and client

---
+++ index-ws.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const server = require('http').createServer();
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+
+});
+
+server.on('request', app);
+server.listen(3000, function() {
+    console.log('Server started on Port 3000');
+});
+
+/** Begin WebSocket*/
+
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on('connection', function connection(ws) {
+    const numClients = wss.clients.size;
+    console.log('New client connected. Total clients: ' + numClients);
+
+    wss.broadcast(`Current visitor  count: ${numClients}`);
+
+    if (ws.readyState === ws.OPEN) {
+        ws.send('Welcome new client!');
+    }
+
+    ws.on('close', function close() {
+        wss.broadcast(`Current visitor  count: ${wss.clients.size}`);
+        console.log('Client disconnected!');
+    });
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    });
+};
